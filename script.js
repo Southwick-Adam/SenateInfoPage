3d57e1710a6564307d58e4927a74c55c3729c416
Adam Southwick <adamsouthwick@pc-134-91.customer.ask4.lan>
2023-10-17 16:54:14 +0100

    added code comments

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ fetch("./senators.json")
     }
     return response.json(); //parse JSON data
   })
-  .then((data) => funcc(data))
+  .then((data) => func(data)) //if everything is good send JSON objects to func
   .catch((error) => {
     console.error("Error:", error); //send the error to the console
   });
 
-let senArr = [];
-let leaderArr = [];
-let parties = {};
+//3 globals
+let senArr = []; //array for holding new senator obj
+let leaderArr = []; //array for holding new leader obj
+let parties = {}; //object to act like dictionary and hold party names as keys and counts as values.
 
+//new senator class with all info we need to load onto page
 class senator {
   constructor(
     name,
@@ -33,7 +35,7 @@ class senator {
     this.party = party;
     this.state = state;
     this.gender = gender;
-    this.gender = rank;
+    this.rank = rank;
     this.office = office;
     this.birthday = birthday;
     this.startdate = startdate;
@@ -43,6 +45,7 @@ class senator {
   }
 }
 
+//new leader class with all info we need to load onto page
 class leader {
   constructor(leadership_title, name, party) {
     this.leadership_title = leadership_title;
@@ -51,20 +54,24 @@ class leader {
   }
 }
 
-function funcc(data) {
+//function we send JSON objects to
+function func(data) {
   for (let s of data.objects) {
     //each s is a senator object
+    //we send JSON data to functions that populate our 3 globals
     partyCount(s.party);
     if (s.leadership_title != null) {
       fillLeaderArr(s);
     }
     fillSenArr(s);
   }
+  //these 3 functions use global arrays/objects so no need to pass anything in
   generatePartyList();
   generateLeaderList();
   generateSenatorList();
 }
 
+//creates object attributes for each party and has Senator count as value
 function partyCount(p) {
   if (Object.keys(parties).includes(p)) {
     parties[p] += 1;
@@ -73,6 +80,7 @@ function partyCount(p) {
   }
 }
 
+//passes in JSON data to create new Senator objects and pushes to senArr
 function fillSenArr(s) {
   let name = s.person.firstname + " " + s.person.lastname;
   const sen = new senator(
@@ -80,7 +88,7 @@ function fillSenArr(s) {
     s.party,
     s.state,
     s.person.gender,
-    s.rank,
+    s.senator_rank,
     s.extra.office,
     s.person.birthday,
     s.startdate,
@@ -91,12 +99,14 @@ function fillSenArr(s) {
   senArr.push(sen);
 }
 
+//passes in JSON data to create new Leader objects and pushes to leaderArr
 function fillLeaderArr(s) {
   let name = s.person.firstname + " " + s.person.lastname;
   const l = new leader(s.leadership_title, name, s.party);
   leaderArr.push(l);
 }
 
+//creates html elements for party div
 function generatePartyList() {
   for (p in parties) {
     const newDiv = document.createElement("div");
@@ -110,37 +120,47 @@ function generatePartyList() {
   }
 }
 
+//creates html info table for leader div
 function generateLeaderList() {
   for (l of leaderArr) {
     //each l is a leader
     const newRow = document.createElement("tr");
-    for (i in l) {
-      //each i is an attribute
+    for (a in l) {
+      //each a is an attribute
       const newEntry = document.createElement("td");
-      newEntry.innerHTML = l[i];
+      newEntry.innerHTML = l[a];
       newRow.appendChild(newEntry);
     }
     document.getElementById("lTable").appendChild(newRow);
   }
 }
 
+//creates html info table for senator div
 function generateSenatorList() {
   for (s of senArr) {
     //each s is a senator
     const newRow = document.createElement("tr");
-    for (i in s) {
-      //each i is an attribute
+    newRow.addEventListener("click", senSelect(s)); //adding an onclick to select senators //DOES NOT WORK YET!!!!!----------------
+    for (a in s) {
+      //each a is an attribute
       const newEntry = document.createElement("td");
-      if (i == "website") {
+      //create a link instead of just write info in for website
+      if (a == "website") {
         const link = document.createElement("a");
+        link.text = s.website;
         link.href = s.website;
-        newEntry.appendChild(link); //this line isnt working for some reason
-        newEntry.innerHTML = link; //just to show the website for now
+        newEntry.appendChild(link);
       } else {
-        newEntry.innerHTML = s[i];
+        newEntry.innerHTML = s[a];
       }
       newRow.appendChild(newEntry);
     }
     document.getElementById("sTable").appendChild(newRow);
   }
+}
+
+//DOES NOT WORK YET!!!!!----------------
+//read sentor data and fill in Info div
+function senSelect(s) {
+  console.log("test");
 }
